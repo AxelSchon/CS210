3679637194cae955626ec31f8918b1cca0978bf3
axelm <ams0190@mix.wvu.edu>
2022-08-26 11:24:46 -0400

    Completed Practice 1 (Converted SearchTable to binary search).

--- src/main/java/tables/SearchTable.java
+++ src/main/java/tables/SearchTable.java
@@ -1,31 +1,27 @@
 package tables;
 
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import sql.FieldType;
 
 /**
- * Implements a search-based table
- * using a list data structure.
+ * Implements a search-based table using a list data structure.
  */
 public class SearchTable extends Table {
-	/*
-	 * TODO: Implement stub for Practice 1 (optional)
-	 * by replacing linear search with binary search.
-	 */
-
-	private List<List<Object>> list;
+	// Map: { key: value, key: value, key: value} --> { "A": 1, "B": 2, "C": 3}
+	// (Rather than indexing, use keys to find information)
+	private Map<Object, List<Object>> tree;
 	private int fingerprint;
 
 	/**
-	 * Creates a table and initializes
-	 * the data structure.
+	 * Creates a table and initializes the data structure.
 	 *
-	 * @param tableName the table name
-	 * @param columnNames the column names
-	 * @param columnTypes the column types
+	 * @param tableName    the table name
+	 * @param columnNames  the column names
+	 * @param columnTypes  the column types
 	 * @param primaryIndex the primary index
 	 */
 	public SearchTable(String tableName, List<String> columnNames, List<FieldType> columnTypes, int primaryIndex) {
@@ -34,13 +30,13 @@ public class SearchTable extends Table {
 		setColumnTypes(columnTypes);
 		setPrimaryIndex(primaryIndex);
 
-		list = new LinkedList<>();
+		tree = new TreeMap<>();
 		clear();
 	}
 
 	@Override
 	public void clear() {
-		list.clear();
+		tree.clear();
 		fingerprint = 0;
 	}
 
@@ -49,45 +45,56 @@ public class SearchTable extends Table {
 		row = sanitizeRow(row);
 
 		Object key = row.get(getPrimaryIndex());
-		for (int i = 0; i < list.size(); i++) {
-			List<Object> old = list.get(i);
-			if (old.get(getPrimaryIndex()).equals(key)) {
-				fingerprint += row.hashCode() - old.hashCode();
-				list.set(i, row);
-				return true;
-			}
+		List<Object> old = tree.put(key, row);
+		if (old != null) {
+			fingerprint += row.hashCode() - old.hashCode();
+			return true;
 		}
 		fingerprint += row.hashCode();
-		list.add(row);
 		return false;
+
+		// old implementation
+		/**
+		 * for (int i = 0; i < list.size(); i++) { List<Object> old = list.get(i); if
+		 * (old.get(getPrimaryIndex()).equals(key)) { fingerprint += row.hashCode() -
+		 * old.hashCode(); list.set(i, row); return true; } } fingerprint +=
+		 * row.hashCode(); list.add(row); return false;
+		 */
 	}
 
 	@Override
 	public boolean remove(Object key) {
-		for (int i = 0; i < list.size(); i++) {
-			List<Object> row = list.get(i);
-			if (row.get(getPrimaryIndex()).equals(key)) {
-				fingerprint -= row.hashCode();
-				list.remove(i);
-				return true;
-			}
+		List<Object> row = tree.remove(key);
+		if (row != null) {
+			fingerprint -= row.hashCode();
+			return true;
 		}
 		return false;
+
+		// old implementation
+		/**
+		 * for (int i = 0; i < list.size(); i++) { List<Object> row = list.get(i); if
+		 * (row.get(getPrimaryIndex()).equals(key)) { fingerprint -= row.hashCode();
+		 * list.remove(i); return true; } } return false;
+		 */
 	}
 
 	@Override
-	public List<Object> get(Object key) {
-		for (List<Object> row: list) {
-			if (row.get(getPrimaryIndex()).equals(key))
-				return row;
-		}
+	public List<Object> get(Object key) {// forwarding method
+		return tree.get(key);
 
-		return null;
+		// old implementation
+		/*
+		 * for (List<Object> row : list) { if (row.get(getPrimaryIndex()).equals(key))
+		 * return row; }
+		 * 
+		 * return null;
+		 */
 	}
 
 	@Override
 	public int size() {
-		return list.size();
+		return tree.size();
 	}
 
 	@Override
@@ -102,18 +109,6 @@ public class SearchTable extends Table {
 
 	@Override
 	public Iterator<List<Object>> iterator() {
-		return new Iterator<>() {
-			int index = 0;
-
-			@Override
-			public boolean hasNext() {
-				return index < list.size();
-			}
-
-			@Override
-			public List<Object> next() {
-				return list.get(index++);
-			}
-		};
+		return tree.values().iterator();
 	}
 }
