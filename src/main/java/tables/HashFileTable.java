aaa32efedebf48eb5e9f93025b21fc5db8e76b82
axelm <ams0190@mix.wvu.edu>
2022-10-06 13:02:50 -0400

    M2 Push for review
    Continued working on module 2. Have strange nesting complication.

--- src/main/java/tables/HashFileTable.java
+++ src/main/java/tables/HashFileTable.java
@@ -16,6 +16,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HexFormat;
 import java.util.Iterator;
@@ -27,18 +28,18 @@ import sql.FieldType;
  * Implements a hash-based table
  * using a directory tree structure.
  */
-public class HashFileTable extends Table {
+public class HashFileTable extends PrettyTable {
 	private Path root;
 	private FileChannel schema, metadata;
-	// field: maximum record width (in his solution: calculate during constructor and store in field) (can be metadata, if you want... harder?)
 
 	// fields for metadata (size, fingerprint) 
 	private int size;
 	private int fingerprint;
 	private int recordWidth;
+
 	// define constants for the limits of the data types
 	private static final int MAX_STRING_LENGTH = 127; // remember to keep track of length (1 byte)
-	private static final int INT_SIZE = 4; // size of int
+	private static final int MAX_INT_SIZE = 4; // size of int
 	private static final int BOOLEAN_SIZE = 1; // 
 	private static final int MAX_COL_NAME_LENGTH = 15; // max length of name
 	private static final int MAX_COL_COUNT = 15; // max number of columns
@@ -58,20 +59,35 @@ public class HashFileTable extends Table {
 	 * @param primaryIndex the primary index
 	 */
 	public HashFileTable(String tableName, List<String> columnNames, List<FieldType> columnTypes, int primaryIndex) {
-		// follow example code to do the following:
-		// assign the root path based on the table name
-		Path root = Paths.get("data", "tables", tableName);
-		// if there is already a root, walk to recursively delete it (suggest delete helper method)
-		deleteMetaData();
-		// create any missing dirs for root path
-		// assign the file channels for both schema and metadata
+		try {
+			// assign the root path based on the table name
+			root = Paths.get("data", "tables", tableName);
+			// if there is already a root, walk to recursively delete it (suggest delete helper method)
+			if (Files.exists(root)) {
+				//replace with delete()?;
+				if (Files.exists(root)) {
+					Files.walk(root).sorted(Comparator.reverseOrder()).map(path -> path.toFile())
+							.forEach(file -> file.delete());
+				}
+			}
+			// create any missing dirs for root path
+			Files.createDirectories(root);
+			// assign the file channels for both schema and metadata
+			schema = FileChannel.open(root.resolve("schema"), CREATE_NEW, READ, WRITE);
+			metadata = FileChannel.open(root.resolve("metadata"), CREATE_NEW, READ, WRITE);
+		} catch (IOException e) {
+			throw new RuntimeException("Schema/mMtaData file I/O error", e);
+		}
 
 		setTableName(tableName);
 		setColumnNames(columnNames);
 		setColumnTypes(columnTypes);
 		setPrimaryIndex(primaryIndex);
+
 		// writeSchema(); // based on the example
-		// measure the maximum width of a record
+		writeSchema();
+
+		recordWidth();
 
 		clear(); // which calls writeMetaData();
 	}
@@ -83,37 +99,25 @@ public class HashFileTable extends Table {
 	 * @param tableName a table name
 	 */
 	public HashFileTable(String tableName) {
-		// follow example code to do the following:
 		// assign the root based on the table name
+		root = Paths.get("data", "tables", tableName);
 		// ensure that the schema and metadata files exist. If not throw exception	
+		if (Files.notExists(root.resolve("schema")) || Files.notExists(root.resolve("schema")))
+			throw new RuntimeException("Missing Schema or MetaData file");
 		// assign the file channels for both schema and metadata
+		try {
+			schema = FileChannel.open(root.resolve("schema"), READ, WRITE);
+			metadata = FileChannel.open(root.resolve("metadata"), READ, WRITE);
+		} catch (IOException e) {
+			throw new RuntimeException("Schema/MetaData file I/O error", e);
+		}
 
 		setTableName(tableName);
 		// readSchema(); // based on example
-		// measure the maximum record width
+		readSchema();
 
-		// readMethadata(); // based on the example
-	}
-
-	@Override
-	public void clear() {
-		truncate(); // empty all rows from table
-		// reset the metadata
-		// writeMetadata();
-	}
-
-	private void truncate() {
-		// resolve the state dir
-		// if state dir  is not there yet, just quit this method
-		// if there is, recursively delete the state folder using a walk (as in the example)
-	}
-
-// optional for M2
-	private void delete() {
-		truncate();
-		// delete metadata file
-		// delete schema file
-		// delete root folder
+		// readMethadata(); // based on the example (record width should come from this since I store it as metadata)
+		readMetaData();
 	}
 
 	@Override
@@ -123,48 +127,53 @@ public class HashFileTable extends Table {
 		var key = row.get(getPrimaryIndex());
 		var digest = digest(key);
 
-		var old = readRecord(digest);
-
+		// check to make sure that what comes back from the put method is the correct row
+		var old = readRecord(digest); // identify hit/miss
+		System.out.println(old);
 		writeRecord(digest, row);
 
-		// update the metadata based on hit/miss
-
-		return false; // based on hit/miss
+		if (old == null) { // miss
+			size++;
+			fingerprint += row.hashCode();
+			writeMetaData();
+			return false;
+		} else { // hit
+			fingerprint += row.hashCode() - old.hashCode();
+			writeMetaData();
+			return true;
+		}
 	}
 
 	@Override
 	public boolean remove(Object key) {
 		// get digest for the key
+		var digest = digest(key);
 
 		// find the existing row (if any) for that digest
+		var row = readRecord(digest);
 		// if it is a miss, terminate method (return false)
-
-		// if we make it here its a hit
-		// delete the corresponding file
-		// update the metadata based on hit/miss
-
-		return true;
-
+		if (row == null) { // miss  
+			return false;
+		} else { // hit
+			// delete the corresponding file
+			try {
+				// should this be pathOf(digest)
+				Files.delete(pathOf(digest));
+			} catch (IOException e) {
+				throw new RuntimeException("Could not remove file", e);
+			}
+			size--;
+			fingerprint -= row.hashCode();
+			recordWidth(); // change so that doesn't have to loop over entire array
+			writeMetaData();
+			return true;
+		}
 	}
 
 	@Override
 	public List<Object> get(Object key) {
 		var digest = digest(key);
-
 		return readRecord(digest);
-	}
-
-	private void writeRecord(String digset, List<Object> row) {
-		//ensure called with valid row
-		// fill in from example 3 code
-	}
-
-	private List<Object> readRecord(String digest) {
-		// fill in from example code
-
-		// return a row on a hit
-		// or else a null on a miss
-		return null;
 	}
 
 	@Override
@@ -175,6 +184,56 @@ public class HashFileTable extends Table {
 	@Override
 	public int capacity() {
 		return size;
+	}
+
+	//wipes out state
+	private void truncate() {
+		// resolve the state dir
+		var state = root.resolve("state");
+		// if state dir  is not there yet, just quit this method
+		if (Files.exists(state)) {
+			;
+		}
+		// if there is, recursively delete the state folder using a walk (as in the example)
+		else {
+			try {
+				if (Files.exists(state)) {
+					Files.walk(state).sorted(Comparator.reverseOrder()).map(path -> path.toFile())
+							.forEach(file -> file.delete());
+				}
+			} catch (IOException e) {
+				throw new RuntimeException("State file I/O error", e);
+			}
+		}
+	}
+
+	@Override
+	public void clear() {
+		// empty all rows from table
+		truncate();
+
+		// reset the metadata
+		size = 0;
+		fingerprint = 0;
+		recordWidth = 0;
+
+		// writeMetadata to file;
+		writeMetaData();
+	}
+
+// optional for M2
+	private void delete() {
+		truncate();
+		try {
+			// delete schema file
+			Files.delete(root.resolve("schema"));
+			// delete metadata file
+			Files.delete(root.resolve("metadata"));
+			// delete root folder
+			Files.delete(root);
+		} catch (IOException e) {
+			throw new RuntimeException("Error deleting Metadata and/or Schema", e);
+		}
 	}
 
 	private String digest(Object key) {
@@ -190,37 +249,43 @@ public class HashFileTable extends Table {
 		} catch (NoSuchAlgorithmException e) {
 			throw new RuntimeException("Digest error", e);
 		}
+
 	}
 
+// convert from digests string to corresponding path 
 // path 12345678abcdef -> "12/345678abcdef"
 	private Path pathOf(String digest) {
 		// get the 2 prefix chars
-		String prefix = digest.substring(0, 3);
+		String prefix = digest.substring(0, 2);
+
 		// get the 38 suffix chars
-		String suffix = digest.substring(3, 39);
+		String suffix = digest.substring(2);
+
 		// return a path from the root
 		// which resolves the prefix
 		// and then resolves the suffix
-
-		//return resolved path
-		return null;
+		return root.resolve("state").resolve(prefix).resolve(suffix);
 	}
 
 // path 12/345678abcdef -> "12345678abcdef"
 	private String digestOf(Path path) {
-		// consult the SHA-1 algorithm to produce a digest
+		StringBuilder sb = new StringBuilder();
 
-		// convert the numeric digest to a hex string
 		// get the folder name (asking the path object for its parent's filename as a string)
+		String folderName = path.getParent().getFileName().toString();
+
 		// get the file name (asking the path object for its filename)
+		String fileName = path.getFileName().toString();
+
 		// concatenate folder and file names together
+		sb.append(folderName).append(fileName);
+		String digestOf = sb.toString();
 
 		//return concatenated names
-		return null;
+		return digestOf;
 	}
 
 	public void deleteMetaData() {
-		Path root = Paths.get("data", "tables", getTableName());
 		try {
 			// DELETE EXISTING TREE
 			if (Files.exists(root)) {
@@ -233,39 +298,34 @@ public class HashFileTable extends Table {
 	}
 
 	public void writeMetaData() {
-		// DUMMY FIELDS
-		Path root = Paths.get("data", "tables", getTableName());
-		FileChannel metadata;
-
 		try {
-			// CREATE NEW TREE
-			Files.createDirectories(root);
-			metadata = FileChannel.open(root.resolve("metadata"), CREATE_NEW, READ, WRITE);
-
 			// WRITE METADATA
-			{
-				var buf = metadata.map(READ_WRITE, 0, INT_SIZE * 3);
-				buf.putInt(size);
-				buf.putInt(fingerprint);
-				buf.putInt(recordWidth);
-			}
+			var buf = metadata.map(READ_WRITE, 0, MAX_INT_SIZE * 3);
+			buf.putInt(size);
+			buf.putInt(fingerprint);
+			buf.putInt(recordWidth);
+		} catch (IOException e) {
+			throw new RuntimeException("Metadata file I/O error", e);
+		}
+	}
+
+	public void readMetaData() {
+		try {
+			// READ METADATA
+			var buf = metadata.map(READ_ONLY, 0, MAX_INT_SIZE * 3);
+			size = (buf.getInt());
+			fingerprint = (buf.getInt());
+			recordWidth = (buf.getInt());
 		} catch (IOException e) {
 			throw new RuntimeException("Metadata file I/O error", e);
 		}
 	}
 
 	public void writeSchema() {
-		// DUMMY FIELDS
-		Path root = Paths.get("data", "tables", getTableName());
-		FileChannel schema;
 		try {
-			// CREATE NEW TREE
-			Files.createDirectories(root);
-			schema = FileChannel.open(root.resolve("schema"), CREATE_NEW, READ, WRITE);
-
 			// WRITE SCHEMA
 			var buf = schema.map(READ_WRITE, 0,
-					INT_SIZE * 2 + (LENGTH_NAME_BYTE + MAX_COL_NAME_LENGTH + LENGTH_TYPE_BYTE) * MAX_COL_COUNT);
+					MAX_INT_SIZE * 2 + (LENGTH_NAME_BYTE + MAX_COL_NAME_LENGTH + LENGTH_TYPE_BYTE) * MAX_COL_COUNT);
 
 			buf.putInt(getColumnNames().size());
 			buf.putInt(getPrimaryIndex());
@@ -278,7 +338,6 @@ public class HashFileTable extends Table {
 				buf.put(new byte[15 - chars.length]);
 
 				var type = getColumnTypes().get(i);
-				;
 				buf.put((byte) type.getTypeNumber());
 
 			}
@@ -287,80 +346,9 @@ public class HashFileTable extends Table {
 		}
 	}
 
-	public void writeState() {
-		// DUMMY FIELDS
-		Path root = Paths.get("data", "tables", getTableName());
-		// DEFINE PATH FROM DIGEST
-
-		try {
-			var path = root.resolve("state").resolve("a1").resolve("b2c3");
-
-			// CREATE NEW TREE
-			Files.createDirectories(path.getParent());
-
-			// WRITE STATE
-			{
-				var channel = FileChannel.open(path, CREATE, READ, WRITE);
-				var buf = channel.map(READ_WRITE, 0,
-						LENGTH_NAME_BYTE + MAX_STRING_LENGTH + LENGTH_INT_BYTE + INT_SIZE + BOOLEAN_SIZE);
-
-				// DUMMY FIELD 0 STRING
-				// WIDTH: 1 + max string length
-				{
-					var str = "alpha"; // DUMMY LETTER VALUE
-					var chars = str.getBytes(UTF_8);
-					buf.put((byte) chars.length);
-					buf.put(chars);
-				}
-
-				// DUMMY FIELD 1 INTEGER-AS-SHORT
-				// WIDTH: 1 + 2
-				{
-					buf.put((byte) Short.BYTES);
-					buf.putShort((short) 1); // DUMMY ORDER VALUE
-				}
-
-				// DUMMY FIELD 2 BOOLEAN
-				// WIDTH: 1
-				{
-					buf.put((byte) 1); // DUMMY VOWEL VALUE
-				}
-			}
-		} catch (IOException e) {
-			throw new RuntimeException("State file I/O error", e);
-		}
-	}
-
-	public void readMetaData() {
-
-		Path root = Paths.get("data", "tables", getTableName());
-		FileChannel metadata;
-
-		try {
-
-			// REOPEN EXISTING TREE
-			if (Files.notExists(root.resolve("metadata")))
-				throw new IOException("Missing metadata file");
-			metadata = FileChannel.open(root.resolve("metadata"), READ);
-
-			// READ METADATA
-			{
-				var buf = metadata.map(READ_ONLY, 0, INT_SIZE * 3);
-
-				size = (buf.getInt());
-				fingerprint = (buf.getInt());
-				recordWidth = (buf.getInt());
-			}
-
-		} catch (IOException e) {
-			throw new RuntimeException("Metadata file I/O error", e);
-		}
-	}
-
 	public void readSchema() {
-		// DUMMY FIELDS
-		Path root = Paths.get("data", "tables", getTableName());
-		FileChannel schema;
+		var colNames = new ArrayList<String>();
+		var colTypes = new ArrayList<FieldType>();
 
 		try {
 			// REOPEN EXISTING TREE
@@ -371,61 +359,63 @@ public class HashFileTable extends Table {
 			// READ SCHEMA
 			{
 				var buf = schema.map(READ_WRITE, 0,
-						INT_SIZE * 2 + (LENGTH_NAME_BYTE + MAX_COL_NAME_LENGTH + LENGTH_TYPE_BYTE) * MAX_COL_COUNT);
+						MAX_INT_SIZE * 2 + (LENGTH_NAME_BYTE + MAX_COL_NAME_LENGTH + LENGTH_TYPE_BYTE) * MAX_COL_COUNT);
 
 				System.out.println(buf.getInt());
-				System.out.println(buf.getInt());
+				setPrimaryIndex(buf.getInt());
 
 				for (int i = 0; i < getColumnNames().size(); i++) {
 					var length = buf.get();
 					var chars = new byte[length];
 					buf.get(chars);
-					System.out.println(new String(chars, UTF_8));
+					colNames.add(new String(chars, UTF_8));
 					buf.get(new byte[MAX_COL_NAME_LENGTH - chars.length]);
-					System.out.println(FieldType.valueOf(buf.get()));
+					colTypes.add(i, FieldType.valueOf(buf.get()));
 				}
 			}
+			setColumnNames(colNames);
+			setColumnTypes(colTypes);
 		} catch (IOException e) {
 			throw new RuntimeException("Schema file I/O error", e);
 		}
 	}
 
-	public void readState() {
-		// DUMMY FIELDS
-		Path root = Paths.get("data", "tables", getTableName());
+	private void writeRecord(String digest, List<Object> row) {
+		//Path to folder containing files: (1st two digest chars)
+		Path fileFolder = root.resolve("state").resolve(digest.substring(0, 2));
 
 		try {
-			// DEFINE PATH FROM DIGEST
-			var path = root.resolve("state").resolve("a1").resolve("b2c3");
-			// REOPEN EXISTING TREE
-			if (Files.notExists(path))
-				throw new IOException("Missing state file");
+			// CREATE NEW TREE
+			Files.createDirectories(fileFolder);
+			// path to files within folder. (last 38 digest chars)
+			Path file = fileFolder.resolve(digest.substring(2));
 
-			// READ STATE
-			{
-				var channel = FileChannel.open(path, READ);
-				var buf = channel.map(READ_ONLY, 0, 1 + 127 + 1 + 4 + 1);
+			// WRITE STATE
+			var channel = FileChannel.open(file, CREATE, READ, WRITE);
+			var buf = channel.map(READ_WRITE, 0,
+					LENGTH_NAME_BYTE + MAX_STRING_LENGTH + LENGTH_INT_BYTE + MAX_INT_SIZE + BOOLEAN_SIZE);
 
-				// DUMMY FIELD 0 STRING
-				// WIDTH: 1 + max string length
-				{
-					var len = buf.get();
-					var chars = new byte[len];
-					buf.get(chars);
-					System.out.println(new String(chars, UTF_8)); // DUMMY LETTER VALUE
-				}
+			// for the number of columns
+			for (int i = 0; i < getColumnNames().size(); i++) {
+				// save that columns type
+				var columnType = getColumnTypes().get(i);
+				// save element at index in row
+				var element = row.get(i);
 
-				// DUMMY FIELD 1 INTEGER-AS-SHORT
-				// WIDTH: 1 + 2
-				{
-					buf.get();
-					System.out.println(buf.getShort()); // DUMMY ORDER VALUE
-				}
-
-				// DUMMY FIELD 2 BOOLEAN
-				// WIDTH: 1
-				{
-					System.out.println(buf.get() == 1); // DUMMY VOWEL VALUE
+				if (columnType == FieldType.STRING) {
+					var str = row.toString();
+					var chars = str.getBytes(UTF_8);
+					buf.put((byte) chars.length);
+					buf.put(chars);
+				} else if (columnType == FieldType.INTEGER) { // need if statement for					
+					buf.put((byte) Short.BYTES);
+					buf.putShort((short) 1);
+				} else if (columnType == FieldType.BOOLEAN) {
+					if ((boolean) element == true) {
+						buf.put((byte) 1);
+					} else if ((boolean) element == false) {
+						buf.put((byte) 0);
+					}
 				}
 			}
 		} catch (IOException e) {
@@ -433,14 +423,82 @@ public class HashFileTable extends Table {
 		}
 	}
 
+	private List<Object> readRecord(String digest) {
+		// DUMMY FIELDS
+		Path fileFolder = root.resolve("state").resolve(digest.substring(0, 2));
+
+		var record = new ArrayList<Object>();
+
+		try {
+			// CREATE NEW TREE
+			Files.createDirectories(fileFolder);
+			// path to files within folder. (last 38 digest chars)
+			Path file = fileFolder.resolve(digest.substring(2));
+
+			// DEFINE PATH FROM DIGEST
+			//var path = pathOf(digest);
+
+			// REOPEN EXISTING TREE
+			if (Files.notExists(file))
+				return null;
+			// READ STATE
+			{
+				var channel = FileChannel.open(file, READ);
+				var buf = channel.map(READ_ONLY, 0, 1 + 127 + 1 + 4 + 1);
+
+				for (int i = 0; i < getColumnNames().size(); i++) {
+					// save that columns type
+					var columnType = getColumnTypes().get(i);
+
+					if (columnType == FieldType.STRING) {
+						var len = buf.get();
+						var chars = new byte[len];
+						buf.get(chars);
+						record.add(new String(chars, UTF_8));
+					} else if (columnType == FieldType.INTEGER) {
+						var size = buf.get();
+						if (size == 4)
+							record.add(buf.getInt());
+						else if (size == 2)
+							record.add((int) buf.getShort());
+						else
+							record.add((int) buf.get());
+					} else if (columnType == FieldType.BOOLEAN) {
+						// TODO check byte to determine boolean
+						record.add(buf.get());
+					}
+					System.out.println(record);
+				}
+				return record;
+				//ensure called with valid row
+			}
+		} catch (IOException e) {
+			throw new RuntimeException("State file I/O error", e);
+		}
+
+		// return a row on a hit
+		// or else a null on a miss
+	}
+
 	@Override
 	public int hashCode() {
 		return fingerprint;
 	}
 
+	// measure the maximum width of a record
+	//	 need to know columnTypes() to perform
+	//	 looping through each columnYype and asking is it a string? then add this many bytes to record width etc.
 	public void recordWidth() {
-		for (int i = 0; i < MAX_COL_COUNT; i++) {
-
+		recordWidth = 0;
+		for (int i = 0; i < getColumnTypes().size(); i++) {
+			var columnType = getColumnTypes().get(i);
+			if (columnType == FieldType.STRING) {
+				recordWidth += 1 + 127;
+			} else if (columnType == FieldType.INTEGER) {
+				recordWidth += 1 + 4;
+			} else if (columnType == FieldType.BOOLEAN) {
+				recordWidth += 1;
+			}
 		}
 	}
 
